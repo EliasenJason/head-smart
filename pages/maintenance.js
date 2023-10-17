e7ae8b0413d70745a952d1d81e640f7061edbef0
Jason Eliasen <eliasenjason@gmail.com>
2023-10-17 01:27:34 -0600

    bug

--- pages/maintenance.js
+++ pages/maintenance.js
@@ -1,5 +1,65 @@
-export default function newPage() {
+import styled from "styled-components"
+import Title from "../components/title"
+import Link from "next/link"
+import { useState, useEffect } from "react";
+// import Job from "../components/maintenance/job";
+
+const JobButton = styled.div`
+  font-size: 1.5rem;
+  text-align: center;
+  border: solid black;
+  margin: 1em;
+`
+
+export default function Maintenance() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedJob, setSelectedJob] = useState('')
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('/api/getJobs');
+        if (response.ok) {
+          const data = await response.json();
+          setData(data);
+          setLoading(false);
+        } else {
+          console.error('Error fetching data:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <p>WORK!</p>
+    <>
+    <p>work?</p>
+      <Title backButtonHref={"/"} Text={'Maintenance'}/>
+      {selectedJob ? (
+        // <Job job={selectedJob} back={setSelectedJob} />
+        <p>selected</p>
+      ) : (
+      <>
+      {loading ? (
+        <p>Loading Jobs...</p>
+      ): (
+        <div>
+            {data.map((item, index) => {
+              return (
+                <JobButton key={index} onClick={() => setSelectedJob(item)}>
+                    <p>{item.jobNumber}</p>
+                </JobButton>
+              )
+            })}
+        </div>
+      )}
+      <Link href="/maintenance/createjob"><button>Create New Job</button></Link>
+      </>
+      )}
+    </>
   )
 }
