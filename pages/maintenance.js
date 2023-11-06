da4367099694a74f03ac87f0c901e9c85ab4301f
Jason Eliasen <eliasenjason@gmail.com>
2023-11-06 02:47:25 -0700

    removed import for file that doesn't exist

--- pages/maintenance.js
+++ pages/maintenance.js
@@ -2,35 +2,26 @@ import styled from "styled-components"
 import Title from "../components/title"
 import Link from "next/link"
 import { useState, useEffect } from "react";
-import Job from "../components/maintenance/job";
 import { useRouter } from "next/router";
 
-const JobButton = styled.div`
-  font-size: 1.5rem;
-  text-align: center;
-  border: solid black;
-  margin: 1em;
-`
-
 export default function Maintenance() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [selectedJob, setSelectedJob] = useState('')
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch('/api/getJobs');
+        const response = await fetch('/api/getJobs')
         if (response.ok) {
           const data = await response.json();
           console.log(data)
           setData(data);
           setLoading(false);
         } else {
-          console.error('Error fetching data:', response.statusText);
+          console.error('Error fetching data:', response.statusText)
         }
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error)
       }
     }
 
